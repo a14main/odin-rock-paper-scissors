22cb534d84c477842c00c1d8bd75323d1d05537f
Andrew Quartermain <quartermain.andrew@gmail.com>
2022-10-28 10:40:41 +0800

    Complete RPS project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
-for (let i = 0; i < 5; i++) {
-    console.log(playRound("rock", getComputerChoice()));
+game();
+
+function game() {
+    let score = 0;
+    for (let i = 0; i < 5; i++) {
+        score += playRound(getPlayerChoice(), getComputerChoice());
+    }
+    if (score < 0) {
+        console.log("Sorry, you lost...");
+    } else if (score > 0) {
+        console.log("Hooray! You won!");
+    } else {
+        console.log("It's a tie...");
+    }
 }
 
 function getComputerChoice() {
@@ -14,56 +26,64 @@ function getPlayerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
-    const win = `You win! ${playerSelection} beats ${computerSelection}`;
-    const lose = `You lose! ${computerSelection} beats ${playerSelection}`;
-    const tie = `It's a tie. You both chose ${playerSelection}`;
-    const invalid = `${playerSelection} is not a valid choice.`;
+    const winText = `You win! ${playerSelection} beats ${computerSelection}`;
+    const loseText = `You lose! ${computerSelection} beats ${playerSelection}`;
+    const tieText = `It's a tie. You both chose ${playerSelection}`;
+    const invalidText = `${playerSelection} is not a valid choice.`;
+
+    const win = 1;
+    const lose = -1;
+    const tie = 0;
+    const invalid = 0;
 
     switch (playerSelection) {
         case "ROCK":
             switch (computerSelection) {
                 case "ROCK":
+                    console.log(tieText);
                     return tie;
                     break;
                 case "PAPER":
+                    console.log(loseText);
                     return lose;
                     break;
                 case "SCISSORS":
+                    console.log(winText);
                     return win;
-                    break;
-                default:
-                    return invalid;
             }
             break;
         case "PAPER":
             switch (computerSelection) {
                 case "ROCK":
+                    console.log(winText);
                     return win;
                     break;
                 case "PAPER":
+                    console.log(tieText);
                     return tie;
                     break;
                 case "SCISSORS":
+                    console.log(loseText);
                     return lose;
-                    break;
-                default:
             }
             break;
         case "SCISSORS":
             switch (computerSelection) {
                 case "ROCK":
+                    console.log(loseText);
                     return lose;
                     break;
                 case "PAPER":
+                    console.log(winText);
                     return win;
                     break;
                 case "SCISSORS":
+                    console.log(tieText);
                     return tie;
-                    break;
-                default:
             }
             break;
         default:
+            console.log(invalidText);
             return invalid;
     }
 }
